29c1022af286c8aa79396ab120009ad6791556fd
Bryan Bell <bryanbellabc@gmail.com>
2021-03-02 15:53:50 -0600

    El counter ahora refleja el gap de un contenedor

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const output = document.querySelector("h1");
 const btnSUM = document.querySelector(".sum");
 const btnRest = document.querySelector(".rest");
 const btnAgain = document.querySelector(".again");
+const gridContainer = document.querySelector(".second");
+
+gridContainer.style.gap = parseInt(output.textContent) + "px";
+
+
 
 const add = () => {
     output.textContent = parseInt(output.textContent) + 1;
+    gridContainer.style.gap = parseInt(output.textContent) +"px";
 }
 
 const rest = () => {
     output.textContent = parseInt(output.textContent) - 1;
+    gridContainer.style.gap = parseInt(output.textContent) +"px";
 }
 
 const again = () => {
-    output.textContent = 0;
+    output.textContent = 1;
+    gridContainer.style.gap = parseInt(output.textContent) +"px";
 }
 
 btnSUM.addEventListener("click", add)
 btnRest.addEventListener("click", rest)
 btnAgain.addEventListener("click", again)
+
+
